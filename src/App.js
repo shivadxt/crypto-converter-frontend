a92db7135608139a50e5548d0df9901135e5e60c
shivadxt <shivamweb3.0@gmail.com>
2023-12-31 15:15:30 +0530

    UI enhnced and added App.css

--- src/App.js
+++ src/App.js
@@ -1,87 +1,76 @@
-import {useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 
 const Converter = () => {
   const [sourceCryptos, setSourceCryptos] = useState([]);
-  const [selectedCrypto, setSelectedCrypto] = useState("");
+  const [selectedCrypto, setSelectedCrypto] = useState('');
   const [amount, setAmount] = useState('');
   const [targetCurrency, setTargetCurrency] = useState('usd');
   const [convertedAmount, setConvertedAmount] = useState('');
   const [error, setError] = useState(null);
 
-  const fetchData = () => {
-    
-    const backendURL = 'https://crypto-backend-project-nodejs.vercel.app/top100'; 
-    
-    axios.get(backendURL)
-      .then(response => {
-        setSourceCryptos(response.data);
-        setError(null);
-      })
-      .catch(error => {
-        setError('Wait we are fetching cryptocurrencies it will take some time :) ');
-      });
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('https://crypto-backend-project-nodejs.vercel.app/top100');
+      setSourceCryptos(response.data);
+      setError(null);
+    } catch (error) {
+      setError('Fetching cryptocurrencies failed. Please try again later.');
+    }
   };
-
 
   useEffect(() => {
     fetchData();
-  }, []); 
+  }, []);
 
-  const handleConversion = () => {
+  const handleConversion = async (e) => {
+    e.preventDefault();
+
     if (amount && sourceCryptos.length > 0) {
-  
-      axios.get(`https://crypto-backend-project-nodejs.vercel.app/convert/${selectedCrypto}/${amount}/${targetCurrency}`)
-        .then(response => {
-          setConvertedAmount(response.data.convertedAmount);
-          setError(null);
-        })
-        .catch(error => {
-          setError('Error converting currency');
-        });
+      try {
+        const response = await axios.get(`https://crypto-backend-project-nodejs.vercel.app/convert/${selectedCrypto}/${amount}/${targetCurrency}`);
+        setConvertedAmount(response.data.convertedAmount);
+        setError(null);
+      } catch (error) {
+        setError('Error converting currency. Please try again.');
+      }
     } else {
-      setError('Please enter valid amount and select a cryptocurrency');
+      setError('Please enter a valid amount and select a cryptocurrency');
     }
   };
 
   return (
     <div className='container'>
       <h1>Currency Converter</h1>
-      {error && <p>Error: {error}</p>}
-      <form onSubmit={e => { e.preventDefault(); handleConversion(); }}>
-      <div style={{paddingBottom:"1rem"}}>
-      <label>
-          Select Source Cryptocurrency:
-          <select onChange={e => setSelectedCrypto(e.target.value)}>
-            {sourceCryptos&&sourceCryptos?.map(crypto => (
+      {error && <p className='error-msg'>{error}</p>}
+      <form onSubmit={handleConversion} className='form'>
+        <div className='form-group'>
+          <label htmlFor='selectCrypto'>Select Source Cryptocurrency:</label>
+          <select id='selectCrypto' onChange={e => setSelectedCrypto(e.target.value)}>
+            {sourceCryptos && sourceCryptos.map(crypto => (
               <option key={crypto.id} value={crypto.id}>{crypto.name}</option>
-            
             ))}
           </select>
-        </label>
         </div>
-        <br />
-        <div style={{paddingBottom:"1rem"}}>
-        <label>
-          Enter Amount:
-          <input type="number" value={amount} onChange={e => setAmount(e.target.value)} />
-        </label>
+
+        <div className='form-group'>
+          <label htmlFor='enterAmount'>Enter Amount:</label>
+          <input id='enterAmount' type="number" value={amount} onChange={e => setAmount(e.target.value)} />
         </div>
-        <br/>
-        <div style={{paddingBottom:"1rem"}}>
-        <label>
-          Select Target Currency:
-          <select onChange={e => setTargetCurrency(e.target.value)} defaultValue="usd">
+
+        <div className='form-group'>
+          <label htmlFor='selectCurrency'>Select Target Currency:</label>
+          <select id='selectCurrency' onChange={e => setTargetCurrency(e.target.value)} defaultValue="usd">
             <option value="usd">USD</option>
             <option value="eur">EUR</option>
           </select>
-        </label>
         </div>
-        <br />
-        <button type="submit">Convert</button>
+
+        <button type="submit" className='convert-btn'>Convert</button>
       </form>
-      {convertedAmount && <p>Converted Amount: {convertedAmount}</p>}
+
+      {convertedAmount && <p className='converted-amount'>Converted Amount: {convertedAmount}</p>}
     </div>
   );
 };
